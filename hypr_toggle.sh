01062a3c1b49bc194d5c65e378272e58abcfeb7a
isGoodSoup <sargent.pointed453@passinbox.com>
2025-06-26 12:43:40 +0200

    hypr_toggle.sh

---
+++ hypr_toggle.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+HOME_DIR=$HOME/dotfiles/hypr/.config/hypr
+
+mv $HOME_DIR/hyprlock.conf $HOME_DIR/.tmp/
+mv $HOME_DIR/hyprpaper.conf $HOME_DIR/.tmp/
+
+mv $HOME_DIR/copies/hyprlock.conf $HOME_DIR
+mv $HOME_DIR/copies/hyprpaper.conf $HOME_DIR
+
+mv $HOME_DIR/.tmp/hyprlock.conf $HOME_DIR/copies/
+mv $HOME_DIR/.tmp/hyprpaper.conf $HOME_DIR/copies/
