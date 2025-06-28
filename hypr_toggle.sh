57e668a3325f2e6993dfef2e3e34f1a013cabed9
isGoodSoup <microsoft.delusion983@passinbox.com>
2025-06-28 02:54:26 +0200

    hypr_toggle.sh

--- hypr_toggle.sh
+++ hypr_toggle.sh
@@ -2,11 +2,11 @@
 
 HOME_DIR=$HOME/dotfiles/hypr/.config/hypr
 
-mv $HOME_DIR/hyprlock.conf $HOME_DIR/.tmp/
-mv $HOME_DIR/hyprpaper.conf $HOME_DIR/.tmp/
+mv $HOME_DIR/hyprlock.conf $HOME_DIR/temp_folder/
+mv $HOME_DIR/hyprpaper.conf $HOME_DIR/temp_folder/
 
 mv $HOME_DIR/copies/hyprlock.conf $HOME_DIR
 mv $HOME_DIR/copies/hyprpaper.conf $HOME_DIR
 
-mv $HOME_DIR/.tmp/hyprlock.conf $HOME_DIR/copies/
-mv $HOME_DIR/.tmp/hyprpaper.conf $HOME_DIR/copies/
+mv $HOME_DIR/temp_folder/hyprlock.conf $HOME_DIR/copies/
+mv $HOME_DIR/temp_folder/hyprpaper.conf $HOME_DIR/copies/
