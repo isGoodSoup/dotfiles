558d9707f0afe24abba0b7b6fc5c6433897d46a8
isGoodSoup <sargent.pointed453@passinbox.com>
2025-11-17 16:01:31 +0100

    update: modified

--- sql.sh
+++ sql.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 source .env
-docker exec -i oracle21 sqlplus $ORACLE_USER/$ORACLE_PASSWORD@XE
+docker container start oracle21
+docker exec -i oracle21 sqlplus "$ORACLE_USER/$ORACLE_PASSWORD@//localhost:1521/XEPDB1"
