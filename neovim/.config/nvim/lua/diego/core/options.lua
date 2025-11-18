5c82f6ef7713601fe78cff86c1c9b31b7c9b6f27
isGoodSoup <sargent.pointed453@passinbox.com>
2025-11-18 02:10:53 +0100

    update: absolute instead of relative numbers

--- neovim/.config/nvim/lua/diego/core/options.lua
+++ neovim/.config/nvim/lua/diego/core/options.lua
@@ -1,6 +1,7 @@
 vim.cmd("let g:netrw_liststyle = 3")
 local opt = vim.opt
-opt.relativenumber = true
+opt.number = true
+opt.relativenumber = false
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs
