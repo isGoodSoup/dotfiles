29cd5f0fd8ec429cf5cd3b8dd8ffaec5c6d690e6
isGoodSoup <sargent.pointed453@passinbox.com>
2025-11-19 11:23:52 +0100

    update: nitch.sh

--- nitch.sh
+++ nitch.sh
@@ -9,18 +9,19 @@ echo "Nitch built successfully"
 sleep 1
 [ -f nitch ] || { echo "ERROR: Build failed"; exit 1; }
 sudo mv nitch /usr/local/bin/
-
+sleep 1
 if which nitch >/dev/null 2>&1; then
     echo "Nitch installed successfully" 
 else 
     echo "ERROR: Couldn't complete operation, Nitch failed to install"
+    sleep 1
     exit
 fi
 cd $HOME
 sleep 1
-echo "Cleaning files"
-sleep 1
+sudo "Cleaning files..."
 sudo rm -r ~/nitch
+sleep 1
 echo "Exiting script"
 sleep 1
 exit 0
